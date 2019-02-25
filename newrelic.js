a4647b110bf206e0153e29db0f80c4580503561e
Onoja Treasure <onojatreasure@gmail.com>
2019-02-25 12:02:26 +0100

    editted newrelic agent app name

--- newrelic.js
+++ newrelic.js
@@ -9,7 +9,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['Face Recognition Brain'],
+  app_name: ['facerecognitionbrain'],
   /**
    * Your New Relic license key.
    */
